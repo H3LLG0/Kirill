0accb1df25ea55d3c80afe610e902769a52969ed
H3LLG0 <126647399+H3LLG0@users.noreply.github.com>
2025-06-18 21:19:15 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,21 +10,35 @@ const path = require('path')
 const PORT = process.env.PORT;
 
 const app = express();
-const corsOptions = {
-    origin: 'https://excellently-spruce-flycatcher.cloudpub.ru/',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'], 
-    credentials: true,
-    maxAge: 86400
-  };
+
 
 app.use(cors(corsOptions));
+
+const corsOptions = {
+    origin: 'https://excellently-spruce-flycatcher.cloudpub.ru',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
+    credentials: true,
+    optionsSuccessStatus: 200
+  };
+app.options('*', cors(corsOptions));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://excellently-spruce-flycatcher.cloudpub.ru');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  
+  next();
+});
 app.use(express.static(path.resolve(__dirname, 'static')));
 app.use(expressFileUploud({}))
 app.use(express.json());
-app.use('/api', router)
-
-
+app.use('/api', router);
 
 const serve = async () => {
     try {
