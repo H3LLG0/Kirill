b98940569f8294764660f9927cacaec1d66d5a73
H3LLG0 <126647399+H3LLG0@users.noreply.github.com>
2025-06-19 22:24:12 +0300

    quiz

--- server/index.js
+++ server/index.js
@@ -12,27 +12,7 @@ const PORT = process.env.PORT;
 const app = express();
 
 
-const corsOptions = {
-    origin: 'https://excellently-spruce-flycatcher.cloudpub.ru',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
-    credentials: true,
-    optionsSuccessStatus: 200
-  };
-app.options('*', cors(corsOptions));
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://excellently-spruce-flycatcher.cloudpub.ru');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
-  
-  next();
-});
+app.use(cors())
 app.use(express.static(path.resolve(__dirname, 'static')));
 app.use(expressFileUploud({}))
 app.use(express.json());
